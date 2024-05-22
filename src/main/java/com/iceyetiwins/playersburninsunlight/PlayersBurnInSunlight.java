f7c0bb3fda1eda1752d7c20fac8b2577ecb054f6
Ethan Betz <ethanxbetz@gmail.com>
2024-05-21 20:32:42 -0400

    Version 1.0

---
+++ src/main/java/com/iceyetiwins/playersburninsunlight/PlayersBurnInSunlight.java
@@ -0,0 +1,14 @@
+package com.iceyetiwins.playersburninsunlight;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PlayersBurnInSunlight implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("players-burn-in-sunlight");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/iceyetiwins/playersburninsunlight/mixin/LivingEntityMixin.java
@@ -0,0 +1,20 @@
+package com.iceyetiwins.playersburninsunlight.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import com.iceyetiwins.playersburninsunlight.OnPlayerUpdate;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+	@Inject(at = @At("RETURN"), method = "tick")
+	private void burninthesun_onTick(CallbackInfo ci) {
+		LivingEntity livingEntity = (LivingEntity) (Object) this;
+		if (livingEntity instanceof PlayerEntity player) {
+			OnPlayerUpdate.onPlayerUpdate(player);
+		}
+	}
+}
