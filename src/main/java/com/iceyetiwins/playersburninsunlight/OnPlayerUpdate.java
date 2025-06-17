922bae8416007873971741298bef3c1a0313a4ca
Ice Yeti <101294194+IceYetiWins@users.noreply.github.com>
2025-06-17 14:44:00 -0400

    Update to 1.21.6

--- src/main/java/com/iceyetiwins/playersburninsunlight/OnPlayerUpdate.java
+++ src/main/java/com/iceyetiwins/playersburninsunlight/OnPlayerUpdate.java
@@ -7,9 +7,9 @@ import net.minecraft.world.biome.Biome.Precipitation;
 
 public class OnPlayerUpdate {
 	public static void onPlayerUpdate(PlayerEntity player){
-		
+
 		BlockPos pos = BlockPos.ofFloored(player.getX(), player.getEyeY(), player.getZ());
-		
+
 		Biome biome = player.getWorld().getBiome(pos).value();
 		boolean isRainingOrSnowing = false;
 
@@ -18,13 +18,9 @@ public class OnPlayerUpdate {
 		while (currentTime >= 24000) {
 			currentTime -= 24000;
 		}
-		
-		if (player.getWorld().isRaining() && ((biome.getPrecipitation(pos, player.getWorld().getSeaLevel()) == Precipitation.RAIN) || (biome.getPrecipitation(pos, player.getWorld().getSeaLevel()) == Precipitation.SNOW))){
-			isRainingOrSnowing = true;
-		} else {
-			isRainingOrSnowing = false;
-		}
-		
+
+		isRainingOrSnowing = player.getWorld().isRaining() && ((biome.getPrecipitation(pos, player.getWorld().getSeaLevel()) == Precipitation.RAIN) || (biome.getPrecipitation(pos, player.getWorld().getSeaLevel()) == Precipitation.SNOW));
+
 		if ((currentTime < 12542 || currentTime > 23460) && player.getWorld().isSkyVisible(pos) && !player.isTouchingWaterOrRain() && !isRainingOrSnowing && !player.inPowderSnow && !player.wasInPowderSnow && !player.isSleeping()){
 			player.setOnFireFor(8);
 		}
